78bb7d05287fff01089989cace8e0b2d0a45b0b5
prajwalbc <75571299+Prajwalbc@users.noreply.github.com>
2023-04-30 22:26:46 +0530

    added recommendation module

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, jsonify
+import pickle
+import requests
+
+app = Flask(__name__)
+
+movie_list_pkl = pickle.load(open('./model/movie_list.pkl','rb'))
+similarity = pickle.load(open('./model/similarity.pkl','rb'))
+
+movieList =  movie_list_pkl['title'].values
+
+# Templates
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+@app.route('/recommend')
+def recommend_page():
+    return render_template("recommend.html")
+
+# APIs
+@app.route('/api/movies')
+def movies_api():
+    return jsonify(movieList.tolist())
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=981e4edeb003b823a185e465213bc596&language=en-US"
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+@app.route('/api/recommend/<movie>', methods=['GET'])
+def recommend_api(movie):
+    try:
+        index = movie_list_pkl[movie_list_pkl['title'] == movie].index[0]
+        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        recommended_movie_names = []
+        recommended_movie_posters = []
+        for i in distances[1:6]:
+            # fetch the movie poster
+            movie_id = movie_list_pkl.iloc[i[0]].movie_id
+            recommended_movie_posters.append(fetch_poster(movie_id))
+            recommended_movie_names.append(movie_list_pkl.iloc[i[0]].title)
+
+        return jsonify(recommended_movie_names, recommended_movie_posters)
+    except:
+        print("**********SERVER ERROR************")
+        return jsonify([])
+
+if __name__ == '__main__':
+    app.run(debug=True)
